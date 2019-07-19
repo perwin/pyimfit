49bd6856356b46fcaee1b16a87efbd3db1e837d4
Peter Erwin <erwin@sigmaxi.net>
2019-07-19 17:32:46 +0200

    Test re-ordering of import in __init__.py

--- pyimfit/__init__.py
+++ pyimfit/__init__.py
@@ -1,10 +1,10 @@
 # This file defines what is visible to the outside world when the package
 # (pyimfit) is imported
+from .descriptions import *
 from .pyimfit_lib import convolve_image, make_imfit_function, ModelObjectWrapper
 from .pyimfit_lib import FixImage
 from .pyimfit_lib import PsfOversampling
 from .pyimfit_lib import get_function_list, get_function_dict
-from .descriptions import *
 from .config import *
 from .fitting import *
 from .psf import *
