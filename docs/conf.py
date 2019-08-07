108b478b93992a09bd25e32ca950e307c2755772
Peter Erwin <erwin@sigmaxi.net>
2019-08-07 19:12:51 +0200

    Updated documentation to v0.8.5

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'Peter Erwin'
 # The short X.Y version
 version = '0.8'
 # The full version, including alpha/beta/rc tags
-release = '0.8.4'
+release = '0.8.5'
 
 
 # -- General configuration ---------------------------------------------------
