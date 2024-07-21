9315a7db323648d0a933458638335a24ed86f588
Peter Erwin <erwin@sigmaxi.net>
2024-07-21 11:52:17 +0200

    Merged scripts for testing conda installs into single, multi-arch script

--- conda/test_conda_installs.sh
+++ conda/test_conda_installs.sh
@@ -3,10 +3,27 @@
 # Shell script to test installation of all PyImfit conda packages
 # (for this architecture)
 
-# magic command necessary to get conda working within a shell script
-source ~/miniconda3/etc/profile.d/conda.sh
+OS=$(uname -s)
 
-cd $HOME/coding/pyimfit/conda
+if [[ "$OS" -eq "Darwin" ]]   # macOS
+then
+  WORKING_DIR="$HOME/coding/pyimfit/conda"
+  # Figure out which type of macOS architecture we're running under, so we know
+  # how to set up conda within this script
+  ARCH=$(uname -m)
+  if [[ "$ARCH" -eq "x86_64" ]]
+  then
+    source ~/miniconda3/etc/profile.d/conda.sh
+  else
+    source /opt/miniconda3/etc/profile.d/conda.sh
+  fi
+else   # Linux
+  source ~/miniconda3/etc/profile.d/conda.sh
+  WORKING_DIR="$HOME/build/pyimfit/conda"
+fi
+
+
+cd $WORKING_DIR
 
 # Python 3.10
 conda activate py310_test
